b2f4ded3d7a1d42a755ea874547ed81e589dc271
Nexdoom <43046818+Nexdoom@users.noreply.github.com>
2018-10-22 17:11:36 +0300

    Add files via upload
    refactoring and add check errors

--- main_script.py
+++ main_script.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -72,7 +73,7 @@ dataset_exp = {"name": "karoutas001_t1",
 dataset_calc = {"name": "karoutas001_t1",
                 "dir": CALC_DATA_DIR,
                 "segments_config": ("x**0.2", 1020.0, "x**-1"),
-                "exclude_filters": ((2000.0, 3000.0),)}
+                "exclude_filters": ((2000.0, 3000.0), (1500.0, 900.0),)}
 #               }
 
 
@@ -92,106 +93,98 @@ dataset_calc = {"name": "karoutas001_t1",
 Direction = Enum("Direction", "left right")
 
 
-class MyError(Exception):
+class CommonError(Exception):
     pass
 
 
-def get_nearest_point(data, x_value, direction):
+class RegrError(Exception):
+    pass
+
+
+def get_nearest_point_index(data, x_value, direction):
     if direction is Direction.left:
-        point_index = data[data["x"] <= x_value].x.idxmax()
+        point_index = max(data[x_value >= data["x"]].index.tolist())
     elif direction is Direction.right:
-        point_index = data[data["x"] >= x_value].x.idxmin()
+        point_index = min(data[x_value <= data["x"]].index.tolist())
+
     return point_index
 
 
-#def get_indexes_to_drop(left_bnd, right_bnd, data):
-#    if isinstance(left_bnd, int):
-#        left_point_index = left_bnd
-#    else:
-#        left_point_index = get_nearest_point(data, left_bnd, Direction.right)
-#
-#    if isinstance(right_bnd, int):
-#        right_point_index = right_bnd
-#    else:
-#        right_point_index = get_nearest_point(data, right_bnd, Direction.left)
-#
-#    if (left_point_index and right_point_index) in data.index.tolist():
-#        return (left_point_index, right_point_index)
-#    else:
-#        raise MyError("No points to delete in specified range: {!s}"
-#                      .format((left_bnd, right_bnd)))
-#
-#
-#def get_filtred_data(dataset):
-#    data = dataset["data"]
-#    if "exclude_filters" not in dataset: return data
-#    data.index += 1
-#
-#    try:
-#        for (left_bnd, right_bnd) in dataset["exclude_filters"]:
-#            left_point_index, right_point_index = get_indexes_to_drop(left_bnd, right_bnd, data)
-#            data = data.drop(range(left_point_index,right_point_index+1))
-#        data.reset_index(drop=True, inplace=True)
-#        return data
-#    except(ValueError):
-#        raise MyError("No points to delete in specified range: {!s}".format((left_bnd, right_bnd)))
+def get_point_indexes_to_drop(data, filters):
 
+    point_indexes = []
+    for (initial_left_bnd, initial_right_bnd) in filters:
+        initial_filter_desc = "{!s}".format((initial_left_bnd, initial_right_bnd))
 
-def get_indexes_to_drop(data, filters):
-    try:
-        index_ponit_list = []
-        for (left_bnd, right_bnd) in filters:
-            if isinstance(left_bnd, int):
-                left_point_index = left_bnd
+        try:
+            if isinstance(initial_left_bnd, float):
+                left_bnd = get_nearest_point_index(data, initial_left_bnd, Direction.right)
             else:
-                left_point_index = get_nearest_point(data, left_bnd, Direction.right)
+                left_bnd = initial_left_bnd
 
-            if isinstance(right_bnd, int):
-                right_point_index = right_bnd
+            if isinstance(initial_right_bnd, float):
+                right_bnd = get_nearest_point_index(data, initial_right_bnd, Direction.left)
             else:
-                right_point_index = get_nearest_point(data, right_bnd, Direction.left)
+                right_bnd = initial_right_bnd
 
-            if (left_point_index or right_point_index) in data.index.tolist():
-                print(range(left_point_index, right_point_index+1))
-                index_ponit_list.extend(range(left_point_index, right_point_index+1))
-            else:
-                raise MyError("Filter {0!s} is out of data range. Data has {1!s} points"
-                              .format((left_bnd, right_bnd), max(data.index.tolist())))
-    except(ValueError):
-        raise MyError("No points to delete in specified range: {!s}"
-                      .format((left_bnd, right_bnd)))
+        except(ValueError):
+            raise CommonError("No points to delete in specified range: {}".format(initial_filter_desc))
 
-    index_ponit_list = list(set(index_ponit_list))
-    return index_ponit_list
+        if left_bnd > right_bnd:
+            raise CommonError("Boundaries are not ascending in specified range: {!s}".format(initial_filter_desc))
+
+        if left_bnd == -1:
+            left_point_index = data.index.min()
+        else:
+            left_point_index = left_bnd
+
+        if right_bnd == -1:
+            right_point_index = data.index.max()
+        else:    
+            right_point_index = right_bnd
+
+        if (left_point_index == 0) or (right_point_index == 0):
+            raise CommonError("Zero boundary error. Boundary=0 in specified range: {}".format(initial_filter_desc))
+
+        if (left_point_index < data.index.min()) or (right_point_index > data.index.max()):
+            raise CommonError("Filter {0!s} exceeds data range. Data has {1!s} points"
+                          .format(initial_filter_desc, max(data.index.tolist())))
+
+        point_indexes.extend(range(left_point_index, right_point_index+1))
+
+    point_indexes = list(set(point_indexes))
+    return point_indexes
 
 
 def get_filtred_data(dataset):
     data = dataset["data"]
+
     if "exclude_filters" not in dataset:
         return data
+
+    filters = dataset["exclude_filters"]
+
     data.index += 1
 
-    indexes_points_list = get_indexes_to_drop(data, dataset["exclude_filters"])
-    try:
-        data = data.drop(indexes_points_list)
-    except MyError:
-        print(MyError)
+    point_indexes_to_drop = get_point_indexes_to_drop(data, filters)
+    data = data.drop(point_indexes_to_drop)
+
     data.reset_index(drop=True, inplace=True)
     return data
 
 
-def create_segments(full_data, regr_info):
-    if isinstance(regr_info, str):
-        regr_info = (regr_info,)
+def create_segments(full_data, segments_config):
+    if isinstance(segments_config, str):
+        segments_config = (segments_config,)
 
     full_data_bnd_left = full_data["x"].min()
     full_data_bnd_right = full_data["x"].max()
 
-    x_values = [full_data_bnd_left] +\
-               [float(val) for val in regr_info if not isinstance(val, str)] +\
+    x_values = [full_data_bnd_left] + \
+               [float(val) for val in segments_config if not isinstance(val, str)] + \
                [full_data_bnd_right]
 
-    regr_functions = [val for val in regr_info if isinstance(val, str)]
+    regr_functions = [val for val in segments_config if isinstance(val, str)]
 
     segments = []
     for (index, regr_func) in enumerate(regr_functions):
@@ -216,8 +209,8 @@ def add_data_to_segments(segments, full_data):
 def get_prediction(data_name, regr_func, data, prediction_points):
     regr_result = rg.ols_fit(regr_func, data)
     predictions = rg.get_prediction(regr_result, prediction_points, verbose=True)
-    rg.output_regress_results(data_name, data, regr_result)
-    rg.plot_prediction_data(predictions)
+#    rg.output_regress_results(data_name, data, regr_result)
+#    rg.plot_prediction_data(predictions)
     return predictions
 
 
@@ -254,42 +247,42 @@ if __name__ == "__main__":
     plot_segments(dataset_exp["data"], dataset_exp["segments"])
     plot_segments(dataset_calc["data"], dataset_calc["segments"])
 
-    ziped_segments = zip(dataset_exp["segments"], dataset_calc["segments"])
-
-    segment_num = 0
-    for segment_exp, segment_calc in ziped_segments:
-        prediction_points_exp = segment_exp["segment_data"]["x"]
-        prediction_exp = get_prediction(dataset_exp["name"],
-                                        segment_exp["regr_func"],
-                                        segment_exp["segment_data"],
-                                        prediction_points_exp)
-        prediction_exp["mean"].name = "mean_exp"
-
-        prediction_points_calc = segment_exp["segment_data"]["x"]
-        prediction_calc = get_prediction(dataset_calc["name"],
-                                         segment_calc["regr_func"],
-                                         segment_calc["segment_data"],
-                                         prediction_points_calc)
-        prediction_calc["mean"].name = "mean_calc"
-
-        segment_exp["segment_data"]["y"].name = "exp"
-        segment_calc["segment_data"]["y"].name = "CORTES"
-
-        predictions = pd.concat([prediction_exp["x"],
-                                 segment_calc["segment_data"]["y"],
-                                 segment_exp["segment_data"]["y"],
-                                 prediction_exp["mean"],
-                                 prediction_exp["deriv"],
-                                 prediction_exp["mean_ci"],
-                                 prediction_exp["mean_se"],
-                                 prediction_exp["mean_ci_lower"],
-                                 prediction_exp["mean_ci_upper"],
-                                 prediction_exp["mean_se_lower"],
-                                 prediction_exp["mean_se_upper"]],
-                                axis=1)
-
-        print(predictions)
-
-        segment_num += 1
-        segment_exp_name = "{}_segm_{}".format(dataset_exp["name"], str(segment_num))
-        srv.save_dataframe_csv(OUT_DIR, segment_exp_name, predictions, sep=";")
+#    ziped_segments = zip(dataset_exp["segments"], dataset_calc["segments"])
+#
+#    segment_num = 0
+#    for segment_exp, segment_calc in ziped_segments:
+#        prediction_points_exp = segment_exp["segment_data"]["x"]
+#        prediction_exp = get_prediction(dataset_exp["name"],
+#                                        segment_exp["regr_func"],
+#                                        segment_exp["segment_data"],
+#                                        prediction_points_exp)
+#        prediction_exp["mean"].name = "mean_exp"
+#
+#        prediction_points_calc = segment_exp["segment_data"]["x"]
+#        prediction_calc = get_prediction(dataset_calc["name"],
+#                                         segment_calc["regr_func"],
+#                                         segment_calc["segment_data"],
+#                                         prediction_points_calc)
+#        prediction_calc["mean"].name = "mean_calc"
+#
+#        segment_exp["segment_data"]["y"].name = "exp"
+#        segment_calc["segment_data"]["y"].name = "CORTES"
+#
+#        predictions = pd.concat([prediction_exp["x"],
+#                                 segment_calc["segment_data"]["y"],
+#                                 segment_exp["segment_data"]["y"],
+#                                 prediction_exp["mean"],
+#                                 prediction_exp["deriv"],
+#                                 prediction_exp["mean_ci"],
+#                                 prediction_exp["mean_se"],
+#                                 prediction_exp["mean_ci_lower"],
+#                                 prediction_exp["mean_ci_upper"],
+#                                 prediction_exp["mean_se_lower"],
+#                                 prediction_exp["mean_se_upper"]],
+#                                axis=1)
+#
+#        print(predictions)
+#
+#        segment_num += 1
+#        segment_exp_name = "{}_segm_{}".format(dataset_exp["name"], str(segment_num))
+#        srv.save_dataframe_csv(OUT_DIR, segment_exp_name, predictions, sep=";")
